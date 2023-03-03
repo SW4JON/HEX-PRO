375018b72f60e0d4d370372b8cdfdd163ceb3d00
Swajon Ahmed <91185222+SW4JON@users.noreply.github.com>
2023-03-04 01:06:42 +0600

    Delete Hex-Pro.py

--- Hex-Pro.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system('git pull')
-
-os.system('cd && rm -rf HEX-PRO && git clone --depth=1 https://github.com/SW4JON/HEX-FIRE.git')
-
-os.system('cd && cd HEX-FIRE ;python Hex.py')
