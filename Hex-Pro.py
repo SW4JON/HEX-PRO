6eaa1f5daecefa812392a3fbea27ad81c4849434
Swajon Ahmed <91185222+SW4JON@users.noreply.github.com>
2023-03-03 21:15:11 +0600

    Update Hex-Pro.py

--- Hex-Pro.py
+++ Hex-Pro.py
@@ -2,6 +2,6 @@ import os
 
 os.system('git pull')
 
-os.system('cd && rm -rf HEX-FIRE && git clone --depth=1 https://github.com/SW4JON/HEX-FIRE.git')
+os.system('cd && rm -rf HEX-PRO && git clone --depth=1 https://github.com/SW4JON/HEX-FIRE.git')
 
 os.system('cd && cd HEX-FIRE ;python Hex.py')
